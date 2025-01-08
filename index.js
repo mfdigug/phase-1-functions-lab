20d95869c9aee961befe718fbb93fc7e34a05de9
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-01-08 21:18:09 +1100

    done with functions lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(customerStreetNumber) {
+    return customerStreetNumber >= 42 ? customerStreetNumber - 42 : 42 - customerStreetNumber
+ };
+
+
+
+function distanceFromHqInFeet(customerStreetNumber) {
+    return distanceFromHqInBlocks(customerStreetNumber) * 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+    return (destination - start) * 264;
+    }
+    else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+   if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0;
+   }
+
+   else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) < 2000) {
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+   }
+
+   if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+    return 25;
+   }
+  
+  if (distanceTravelledInFeet(start, destination) >= 2500) {
+    return 'cannot travel that far';
+   }
+}
